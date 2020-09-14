e4c9dd91bc5a89435d959abbaae50c028777191f
Brandon <btran8337@yahoo.com>
2020-09-14 12:26:39 -0500

    wrote integrations test for axios calls using moxios

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,35 +1,40 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import * as actions from 'actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "actions";
 
 class CommentBox extends Component {
-	state = {
-		comment: ''
-	};
+  state = {
+    comment: ""
+  };
 
-	handleChange = (e) => {
-		this.setState({ comment: e.target.value });
-	};
+  handleChange = (e) => {
+    this.setState({ comment: e.target.value });
+  };
 
-	handleSubmit = (e) => {
-		e.preventDefault();
+  handleSubmit = (e) => {
+    e.preventDefault();
 
-		this.props.saveComment(this.state.comment);
+    this.props.saveComment(this.state.comment);
 
-		this.setState({ comment: '' });
-	};
+    this.setState({ comment: "" });
+  };
 
-	render() {
-		return (
-			<form onSubmit={this.handleSubmit}>
-				<h4>Add a Comment</h4>
-				<textarea onChange={this.handleChange} value={this.state.comment} />
-				<div>
-					<button>Submit Comment</button>
-				</div>
-			</form>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <h4>Add a Comment</h4>
+          <textarea onChange={this.handleChange} value={this.state.comment} />
+          <div>
+            <button>Submit Comment</button>
+          </div>
+        </form>
+        <button className='fetch-comments' onClick={this.props.fetchComments}>
+          Fetch Comments
+        </button>
+      </div>
+    );
+  }
 }
 
 export default connect(null, actions)(CommentBox);
--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -1,45 +1,45 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import CommentBox from 'components/CommentBox';
-import Root from 'root';
+import React from "react";
+import { mount } from "enzyme";
+import CommentBox from "components/CommentBox";
+import Root from "root";
 
 let wrapped;
 
 beforeEach(() => {
-	wrapped = mount(
-		<Root>
-			<CommentBox />
-		</Root>
-	);
+  wrapped = mount(
+    <Root>
+      <CommentBox />
+    </Root>
+  );
 });
 
 afterEach(() => {
-	wrapped.unmount();
+  wrapped.unmount();
 });
 
-it('has a text area and a button', () => {
-	expect(wrapped.find('textarea').length).toEqual(1);
-	expect(wrapped.find('button').length).toEqual(1);
+it("has a text area and two button", () => {
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(2);
 });
 
-describe('the text area', () => {
-	beforeEach(() => {
-		wrapped.find('textarea').simulate('change', {
-			target: { value: 'new comment' }
-		});
+describe("the text area", () => {
+  beforeEach(() => {
+    wrapped.find("textarea").simulate("change", {
+      target: { value: "new comment" }
+    });
 
-		wrapped.update();
-	});
+    wrapped.update();
+  });
 
-	it('has a text area that users can type in', () => {
-		expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
-	});
+  it("has a text area that users can type in", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+  });
 
-	it('when form is submitted, text are gets emptied', () => {
-		expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  it("when form is submitted, text are gets emptied", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
 
-		wrapped.find('form').simulate('submit');
-		wrapped.update();
-		expect(wrapped.find('textarea').prop('value')).toEqual('');
-	});
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
 });
